1ddf576a15a795f962b74dc843d559d12c8e065c
Nikita Sufyanov <niksuf1@yandex.ru>
2022-07-01 03:34:07 +0300

    Code format fix

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,11 +1,13 @@
 from random import random, seed
 from time import time
 
+
 def isSorted(array, size):                                          # Проверка на упорядоченность
     for i in range (0, size - 1):
         if array[i] > array[i + 1]:
             return False
     return True
+
 
 def printArray(array, size):                                        # Вывод массива
     if isSorted(array, size):
@@ -18,24 +20,31 @@ def printArray(array, size):                                        # Вывод
         else: break
     print('\n')
 
+
 def sort(array, size):                                              # Сортировка массива
     for i in range (0, size):
         for j in range (0, size - 1):
             if array[j] > array[j + 1]:
                 array[j], array[j + 1] = array[j + 1], array[j]
 
-seed(time())                                                        # Инициализация таймера
-print('Input array size: ')
-size = int(input())                                                 # Размер массива
-random_array = []
-for i in range (0, size):                                           # Заполнение массива случайными числами
-    random_array.append(random() * size)
-printArray(random_array, size)
 
-t1 = time()
-sort(random_array, size)                                            # Сортировка
-t2 = time()
-elapsed = 1000 * (t2-t1)                                            # Время, затраченное на операцию, мс
+def main():
+    seed(time())                                                        # Инициализация таймера
+    print('Input array size: ')
+    size = int(input())                                                 # Размер массива
+    random_array = []
+    for i in range (0, size):                                           # Заполнение массива случайными числами
+        random_array.append(random() * size)
+    printArray(random_array, size)
 
-printArray(random_array, size)
-print('Time: ', elapsed, 'ms')
+    t1 = time()
+    sort(random_array, size)                                            # Сортировка
+    t2 = time()
+    elapsed = 1000 * (t2-t1)                                            # Время, затраченное на операцию, мс
+
+    printArray(random_array, size)
+    print('Time: ', elapsed, 'ms')
+
+
+if __name__ == '__main__':
+    main()
